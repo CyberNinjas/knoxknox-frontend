30740132e8618c74982b088e84dbea309cbed432
Zachary <zachary.standridge.tt@gmail.com>
2017-03-17 14:27:36 -0400

    updated user interface

--- deploy/Login.js
+++ deploy/Login.js
@@ -1,1 +1,10 @@
+angular.module('myApp', [])
+    .controller('loginCtrl', ['$scope', function($scope){
+        $scope.redirect = function() {
+            $location.path('/knoxknox-frontend/deploy/UserControl.html');
+        };
 
+        $scope.test = function() {
+            $scope.testing = 'it works';
+        };
+    }]);
