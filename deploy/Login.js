51ca3a4344319389bf047cd9d07f4adb2cbebeef
zach <ztstandridge@mail.usf.edu>
2017-04-10 14:32:20 -0400

    updated code for login page

--- deploy/Login.js
+++ deploy/Login.js
@@ -1,6 +1,6 @@
 (function() {
    angular
-       .module('loginCtrl')
+       .module('loginCtrl', [])
        .controller("MainCtrl", ['userAccount', MainCtrl]);
 
    function MainCtrl(userAccount) {
@@ -9,16 +9,29 @@
        user.message = '';
        user.userData = {
            userName: '',
+           email: '',
            password: ''
        };
 
        user.loginUser = function() {
            user.userData.grant_type = "password";
+           user.userData.userName =  user.userData.email;
 
            userAccount.loginUser(user.userData,
                 function(data) {
-                    user.message = "registration successful";
-                    user.userData = data;
+                    user.isLoggedIn = true;
+                    user.message = '';
+                    user.password = '';
+                    user.token = data.access_token;
+                },
+                function(response) {
+                    user.password = '';
+                    user.isLoggedIn = false;
+                    user.message = response.statusText + "\r\n";
+                    if(response.data.exceptionMessage)
+                        user.message += response.data.exceptionManager;
+                    if(response.data.error)
+                        user.message += response.data.error;
                 });
        }
    }
--- deploy/userAccount.js
+++ deploy/userAccount.js
@@ -6,7 +6,16 @@
     function userAccount($resource, appServer) {
         return $resource(appServer.serverPath + '/api/Account/Register', null,
             {
-                'loginUser': {method: 'POST'}
+                'loginUser': {
+                    method: 'POST',
+                    headers: {'Content-Type': 'application/x-www-form-urlencoded'},
+                    transformRequest: function(data, headersGetter){
+                        var str = [];
+                        for (var d in data)
+                            str.push(encodeURIComponent(d) + "=" + encodeURIComponent(data[d]));
+                        return str.join("&");
+                    }
+                }
             });
     }
 }());
