7162543c4084695bf574a608a2f6b344b1d6e844
zach <ztstandridge@mail.usf.edu>
2017-04-13 08:24:24 -0400

    updated code for login page, ready for test 4/13/17

--- deploy/Login.js
+++ deploy/Login.js
@@ -1,38 +1,25 @@
-(function() {
-   angular
-       .module('loginCtrl', [])
-       .controller("MainCtrl", ['userAccount', MainCtrl]);
+angular.module('myApp', [])
+    .controller('myCtrl', function($scope, $http) {
 
-   function MainCtrl(userAccount) {
-       var user = this;
-       user.isLoggedIn = false;
-       user.message = '';
-       user.userData = {
-           userName: '',
-           email: '',
-           password: ''
-       };
+        $scope.sendData = function() {
+            //debugger
+            var data = {
+                username: $scope.username,
+                password: $scope.password
+            };
 
-       user.loginUser = function() {
-           user.userData.grant_type = "password";
-           user.userData.userName =  user.userData.email;
+            var config = {
+                headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8;'
+                }
+            };
 
-           userAccount.loginUser(user.userData,
-                function(data) {
-                    user.isLoggedIn = true;
-                    user.message = '';
-                    user.password = '';
-                    user.token = data.access_token;
-                },
-                function(response) {
-                    user.password = '';
-                    user.isLoggedIn = false;
-                    user.message = response.statusText + "\r\n";
-                    if(response.data.exceptionMessage)
-                        user.message += response.data.exceptionManager;
-                    if(response.data.error)
-                        user.message += response.data.error;
+            $http.post('/adminlogin', data, config)
+                .then(function(response) {
+                    if(response.data)
+                        $scope.msg = "Post Data Successfully";
+                }, function(response) {
+                    $scope.msg = "Failure";
                 });
-       }
-   }
-}());
+        };
+    });
